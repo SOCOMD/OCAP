1a4ccc95df0b75ed3fb8961bd546cd56fa1d00ae
Dell <Vova-kyzmin@yandex.ru>
2019-10-19 22:11:01 +0300

    version as on site

--- web/data/receive.php
+++ web/data/receive.php
@@ -7,12 +7,12 @@ if (count($_GET) == 0) {
 	exit;
 }
 
+if (!in_array($_SERVER['REMOTE_ADDR'], $ipGameServer)) die();
+
 if (!file_exists("data.db")) {
 	echo "Database not found. Please ensure you have ran the installer first.";
 	exit;
-};
-if (!in_array($_SERVER['REMOTE_ADDR'], $ipGameServer)) die("Don't hack me! ".$_SERVER["REMOTE_ADDR"]);
-//if (($_SERVER['REMOTE_ADDR'] != "195.88.209.214") && ($_SERVER['REMOTE_ADDR'] != "193.19.118.241")) die("Don't hack me! ".$_SERVER["REMOTE_ADDR"]);
+}
 
 $option = $_GET["option"];
 
@@ -28,43 +28,60 @@ if (strlen($string) > 500) {
 }
 echo "Processed data: \n". $string . "\n";
 
-$forbiddenChar = array("\\", "/", ":", "*", "?", "\*", "<", ">", "|", "\"");
+function str2filename($str) : string
+{
+	//$res = preg_replace('/[\/\\\:\*\?\"\<\>\|\.\+\%\!\@]/', '', $str);
+	$res = preg_replace('/[\0\r\n\\\"\'\/\[\]:;\|=\?\<\>\*\.\,]/', '', $str);
+	return $res;
+}
 
 if ($option == "addFile") { // Add receieved file to this directory
-	// Проверяем лог на имя файла
-	$fileopen=fopen("POST.log", "a+");
-	$write =  date ("Y-m-d H:i:s") . "POST[fileName]:" . $_POST['fileName'] . " urldecode - " . urldecode($_POST["fileName"]) . "\r\n";
-	fwrite($fileopen,$write);
-	fclose($fileopen);
-	
-	$fileName = str_replace($forbiddenChar, "", urldecode($_POST["fileName"]));
-	// $fileContents = $_FILES["fileContents"];
-	$data = file_get_contents($_FILES["fileContents"]['tmp_name']);
-	// try {
-	// 	if (move_uploaded_file($fileContents["tmp_name"], 'no_compress/' . $fileName)) {
-	// 		echo "Successfully created file.";
-	// 	} else {
-	// 		echo "No successfully created file.";
-	// 	};
-	// } catch (Exception $e) {
-	// 	echo $e->getMessage();
-	// };
-	// $data = implode("", file($fileContents["tmp_name"]));
-	$gzdata = gzencode($data, 6);
+	$fileName = str2filename($_POST["fileName"]);
+	$fileContents = $_FILES["fileContents"];
+
+	try {
+		if (move_uploaded_file($fileContents["tmp_name"], $fileName)) {
+			echo "Successfully created file.";
+		} else {
+			echo "No successfully created file.";
+		};
+	} catch (Exception $e) {
+		echo $e->getMessage();
+	}
+	$data = implode("", file($fileName));
+	$gzdata = gzencode($data, 9);
 	$fp = fopen("$fileName.gz", "w");
 	fwrite($fp, $gzdata);
 	fclose($fp);
-	// Delete it if everything works.
-	move_uploaded_file($_FILES["fileContents"]['tmp_name'], 'no_compress/' . $fileName);
 } elseif ($option == "dbInsert") { // Insert values into SQLite database
 	$date = date("Y-m-d");
 	$serverId = -1;
 	try {
 		$db = new PDO('sqlite:data.db');
-		if (!isSet($_GET["type"])) {$type = "other";} else {$type = $_GET["type"];};
+
+		$sql = "INSERT INTO operations 
+			(world_name, mission_name, mission_duration, filename, date, type) VALUES (
+				:world_name,
+				:mission_name,
+				:mission_duration,
+				:filename,
+				:date,
+				:type
+			)
+		";
+		$sth = $db->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$sth->execute(array(
+			':world_name' => $_GET["worldName"],
+			':mission_name' => $_GET["missionName"],
+			':mission_duration' => $_GET["missionDuration"],
+			':filename' => str2filename($_GET["filename"]),
+			':date' => $date,
+			':type' => $_GET["type"]
+		));
+		
 		// Add operation to database
 		/*
-		$db->exec(sprintf("
+		$quer = sprintf("
 			INSERT INTO operations (world_name, mission_name, mission_duration, filename, date, type) VALUES (
 				'%s',
 				'%s',
@@ -73,32 +90,23 @@ if ($option == "addFile") { // Add receieved file to this directory
 				'%s',
 				'%s'
 			)
-		", addslashes($_GET["worldName"]), addslashes($_GET["missionName"]), $_GET["missionDuration"], addslashes($_GET["filename"]), $date, $type));*/
+		", $_GET["worldName"], $_GET["missionName"], $_GET["missionDuration"], str2filename($_GET["filename"]), $date, $_GET["type"]);
+		var_dump($quer);
+		var_dump ($db->exec($quer));
+		*/
 		// TODO: Increment local capture count
-		$stmt = $db -> prepare("INSERT INTO operations (world_name, mission_name, mission_duration, filename, date, type) VALUES (
-				:world_name,
-				:mission_name,
-				:mission_duration,
-				:filename,
-				:date,
-				:type
-			)");
-		$stmt -> execute(array(
-			'world_name' => $_GET["worldName"],
-			'mission_name' => $_GET["missionName"],
-			'mission_duration' => $_GET["missionDuration"],
-			'filename' => str_replace($forbiddenChar, "", $_GET["filename"]),
-			'date' => $date,
-			'type' => $type
-			));
-		
+
+		// Get server ID
+		$results = $db->query("SELECT remote_id FROM servers");
+		$serverId = $results->fetch()["remote_id"];
+		$db = null;
+		print_debug($serverId);
 
 	} catch (PDOExecption $e) {
 		echo "Exception: ".$e->getMessage();
 	}
 
 	// TODO: Increment capture count on remote database
-	
 	$result = curlRemote("stats-manager.php", array(
 		"option" => "increment_capture_count",
 		"server_id" => $serverId
